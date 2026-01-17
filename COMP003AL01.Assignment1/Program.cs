754c455b3f1650ede111e854f9e8194a521007a5
briannaordaz <briannaordaz5@gmail.com>
2026-01-16 21:29:05 -0800

    added items 11-20

--- COMP003AL01.Assignment1/Program.cs
+++ COMP003AL01.Assignment1/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+
+class program
+{
+    
 
 static void Main(string[] args)
 {
@@ -14,6 +19,18 @@ static void Main(string[] args)
     Console.WriteLine("8. I lived in Dallas Texas when I was 7 for a year");
     Console.WriteLine("9. I moved to Los Angeles California for a year");
     Console.WriteLine("10. I moved to San Jose California for a year");
+    Console.WriteLine("11. I've lived in Hanford California for almost 10 years");
+    Console.WriteLine("12. My first pet was a hamster");
+    Console.WriteLine("13. My favorite color is blue");
+    Console.WriteLine("14. I have brown eyes");
+    Console.WriteLine("15. My hair is brown");
+    Console.WriteLine("16. I have 3 classes");
+    Console.WriteLine("17. My favorite school subject is math");
+    Console.WriteLine("18. I like playing video games");
+    Console.WriteLine("19. I like drawing");
+    Console.WriteLine("20. I like to read books");
     
     
+}
+
 }
