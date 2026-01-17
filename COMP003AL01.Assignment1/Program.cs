2f6798b40d2bae52c969f0ee671f794fd1142bb5
briannaordaz <briannaordaz5@gmail.com>
2026-01-16 21:12:54 -0800

    completed the first 10 things about me

--- COMP003AL01.Assignment1/Program.cs
+++ COMP003AL01.Assignment1/Program.cs
@@ -3,5 +3,17 @@
 
 static void Main(string[] args)
 {
-    Console.WriteLine("Hello World!");
+    Console.WriteLine("100 things About Me:");
+    Console.WriteLine("1. I was born in Georgia U.S.A");
+    Console.WriteLine("2. I am 19 years old.");
+    Console.WriteLine("3. I have 3 brothers");
+    Console.WriteLine("4. My birthday is 04.25.2006 ");
+    Console.WriteLine("5. I graduated from highschool in 2024");
+    Console.WriteLine("6. This is my second year at lemoore college");
+    Console.WriteLine("7. I'm majoring in Information Technology");
+    Console.WriteLine("8. I lived in Dallas Texas when I was 7 for a year");
+    Console.WriteLine("9. I moved to Los Angeles California for a year");
+    Console.WriteLine("10. I moved to San Jose California for a year");
+    
+    
 }
